191103bebc346d59582a5028916ab0d24babc315
ShalihRizal <rizalpangandaran9@gmail.com>
2022-08-13 14:25:10 +0800

    space movement

--- Assets/Scripts/SpaceController.cs
+++ Assets/Scripts/SpaceController.cs
@@ -6,8 +6,15 @@ using Agate.MVC.Core;
 
 public class SpaceController : ObjectController<SpaceController, SpaceModel, ISpaceModel, SpaceView>
 {
+
+    float movSpeed = 4f;
     public void OnMoveSpace()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
 
+        Vector2 direction = new Vector2(horizontalInput, 0).normalized;
+
+        _view.transform.Translate(direction * movSpeed * Time.deltaTime);
     }
+
 }
